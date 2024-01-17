9c6a9ceefd03e80adff6c7e98c98f49e890f3142
Nathan Gray <nrg508@york.ac.uk>
2024-01-17 13:37:02 +0000

    minoritised white

--- R/facet_RUC_decile_emissions_ethnicity.R
+++ R/facet_RUC_decile_emissions_ethnicity.R
@@ -19,6 +19,14 @@ data <- read.csv(prawn_path,
 edata <- read.csv("Data/LSOA_statistics/census2021-ts021-lsoa.csv",
                   check.names=FALSE,
                   sep="|") %>%
+  mutate(`Minoritised white`=
+           `Ethnic group: White: Irish`+
+           `Ethnic group: White: Gypsy or Irish Traveller`+
+           `Ethnic group: White: Roma`+
+           `Ethnic group: White: Other White`
+         ) %>% 
+
+
   dplyr::select(c(geography,
                   `geography code`,
                   `Ethnic group: Asian, Asian British or Asian Welsh`,
@@ -26,6 +34,7 @@ edata <- read.csv("Data/LSOA_statistics/census2021-ts021-lsoa.csv",
                   `Ethnic group: Mixed or Multiple ethnic groups`,
                   `Ethnic group: White`,
                   `Ethnic group: Other ethnic group`,
+                  `Minoritised white`,
                   `Ethnic group: Total: All usual residents`)) %>%
   rename(`Asian, Asian British or\nAsian Welsh`=
            `Ethnic group: Asian, Asian British or Asian Welsh`,
@@ -45,7 +54,8 @@ edata <- read.csv("Data/LSOA_statistics/census2021-ts021-lsoa.csv",
       `Black, Black British, \nBlack Welsh, Caribbean\nor African`,
       `Mixed or Multiple \nethnic groups`,
       `White`,
-      `Other ethnic\ngroup`),
+      `Other ethnic\ngroup`,
+      `Minoritised white`),
     names_to = "Ethnic group",
     values_to = "flat population"
   ) %>%
@@ -75,7 +85,11 @@ plottable <- foray %>% inner_join(
     RUC11=="Urban city and town in a sparse setting"~"Urban city and town",
     .default = RUC11)) %>%
   group_by(`Ethnic group`,Diversity_quintile,`Rural urban classification`) %>%
-  summarise(Mean=mean(Total))
+  summarise(Mean=mean(Total))%>% 
+  mutate(linetype=case_when(
+    `Ethnic group`=="Minoritised white"~"dashed",
+    .default="straight"
+  ))
 
 RUC_frequency <- foray %>% inner_join(
   x=data %>% dplyr::select(RUC11,Total,LSOA11CD),
@@ -104,7 +118,8 @@ output <- ggplot(data=plottable
 
   aes(x=Diversity_quintile,
       y=Mean,
-      colour=`Ethnic group`)+
+      colour=`Ethnic group`,
+      linetype=linetype)+
 
   geom_line(stat="summary",
             linewidth=0.5,
@@ -129,8 +144,13 @@ output <- ggplot(data=plottable
   guides(fill = guide_legend(byrow = TRUE))+
 
   facet_wrap(~fct_reorder(`Rural urban classification`,Mean,mean,na.rm=TRUE,.desc=TRUE))+
-  scale_colour_manual(values=c("black","royalblue","olivedrab1","#FB8022FF","deeppink2"))+
+  scale_colour_manual(values=c("black","royalblue","deeppink2","olivedrab1","#FB8022FF","deeppink2"))+
   theme_classic()+
+  scale_linetype_manual(values=c(5,1),)+
+  guides(linetype="none",
+         colour=guide_legend(override.aes = 
+                               list(linetype=c(1,1,5,1,1,1)))
+  )+
   scale_x_continuous(
     breaks=c(1:10),
     expand = expansion(mult=0,add=0.1),
