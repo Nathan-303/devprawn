57f2150ae7c4a8dbad4db2fef87d9994bf31df28
Nathan Gray <nrg508@york.ac.uk>
2024-01-17 11:22:16 +0000

    fixed labels

--- R/facet_RUC_deprivation_emissions_ethnicity.R
+++ R/facet_RUC_deprivation_emissions_ethnicity.R
@@ -91,8 +91,8 @@ output <- ggplot(data=plottable)+
     expand = expansion(mult=0,add=0),
     minor_breaks = FALSE)+
   scale_y_continuous(expand=expansion(mult=c(0,0.06),add=0), limits = c(0, NA))+
-  labs(labs(y=bquote("Average"~.(pollutant)~"emissions/ tonnes "~km^"-2"),
-            x="IMD decile where 10 is least deprived"))
+  labs(y=bquote("Average"~.(pollutant)~"emissions/ tonnes "~km^"-2"),
+            x="IMD decile where 10 is least deprived")
 
 output
 
